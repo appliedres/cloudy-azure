f735199c59d1c2f4acd35b55462652c122cc6897
John Bauer <jbauer@appliedres.com>
2023-03-09 20:11:42 -0500

    Added recover secret to KV

--- keyvault_test.go
+++ keyvault_test.go
@@ -31,31 +31,3 @@ func TestKeyVault(t *testing.T) {
 
 	secrets.SecretsTest(t, ctx, kv)
 }
-
-func TestKeyVaultTemp(t *testing.T) {
-	_ = testutil.LoadEnv("../arkloud-conf/arkloud.env")
-
-	ctx := cloudy.StartContext()
-
-	tenantID := cloudy.ForceEnv("KEYVAULT_AZ_TENANT_ID", "")
-	ClientID := cloudy.ForceEnv("KEYVAULT_AZ_CLIENT_ID", "")
-	ClientSecret := cloudy.ForceEnv("KEYVAULT_AZ_CLIENT_SECRET", "")
-
-	creds := AzureCredentials{
-		TenantID:     tenantID,
-		ClientID:     ClientID,
-		ClientSecret: ClientSecret,
-	}
-	vaultURL := cloudy.ForceEnv("AZ_VAULT_URL", "")
-
-	provider, err := NewKeyVault(ctx, vaultURL, creds)
-	assert.Nil(t, err)
-
-	secStr2, err := provider.GetSecret(ctx, "mytest")
-	assert.Nil(t, err)
-	assert.Equal(t, secStr2, "test")
-
-	err = provider.SaveSecret(ctx, "mytest2", "test2")
-	assert.Nil(t, err)
-
-}
