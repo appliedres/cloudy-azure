b5736d631ce5fe4e985c13cba78c57b045ec3ea6
Joshua Dupras <hello@joshuadupras.com>
2025-04-18 14:01:32 -0400

    Move session host creation

--- vdo/vdo-pooled-avd.go
+++ vdo/vdo-pooled-avd.go
@@ -3,14 +3,11 @@ package vdo
 import (
 	"context"
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/desktopvirtualization/armdesktopvirtualization/v2"
 	"github.com/appliedres/cloudy/logging"
 	cm "github.com/appliedres/cloudy/models"
-	"github.com/appliedres/cloudy"
 )
 
 // TODO: lock session host 'slot' during this process so its not factored in for simultaneous requests
@@ -61,8 +58,7 @@ func (vdo *VirtualDesktopOrchestrator) LinuxAVDPreCreateSetup(ctx context.Contex
 	if bestHost == nil {
 		log.InfoContext(ctx, "LinuxAVDPreCreateSetup - No suitable session host found, creating a new one")
 			
-		sessionHostID := strings.ToLower(fmt.Sprintf("shvm-%s", cloudy.GenerateTimestampIDNow()))
-		bestHost, err = vdo.CreateSessionHost(ctx, sessionHostID, hostPoolName)
+		bestHost, err = vdo.CreateSessionHost(ctx, hostPoolName)
 		if err != nil {
 			log.ErrorContext(ctx, "LinuxAVDPreCreateSetup - Failed to create new session host", "Error", err)
 			return fmt.Errorf("failed to create new session host: %w", err)
@@ -146,68 +142,4 @@ func (vdo *VirtualDesktopOrchestrator) LinuxAVDPostCreateSetup(ctx context.Conte
 
 	log.InfoContext(ctx, "LinuxAVDPostCreateSetup - Completed successfully")
 	return &vm, nil
-}
-
-func (vdo *VirtualDesktopOrchestrator) CreateSessionHost(ctx context.Context, ID, hostPoolName string) (*armdesktopvirtualization.SessionHost, error) {
-	log := logging.GetLogger(ctx)
-	log.InfoContext(ctx, "CreateSessionHost - Starting", "HostPoolName", hostPoolName)
-
-	sessionHostVM := &cm.VirtualMachine{
-		ID: 			ID,
-		Name: 			ID,
-		Description: 	"a session host VM for pooled AVD'",
-		Template: &cm.VirtualMachineTemplate{
-			OperatingSystem:      "windows",
-			OsBaseImageID:        "/subscriptions/58a87bdf-316e-464d-90c5-088c55e78c45/resourceGroups/arkloud-portal-testing-usgt/providers/Microsoft.Compute/galleries/vulcan_test_image_gallery/images/Windows-11-WS/versions/1.0.0",
-			LocalAdministratorID: "salt",
-			Size: &cm.VirtualMachineSize{
-				ID: "Standard_D2s_v4",
-			},
-			SecurityProfile: &cm.VirtualMachineSecurityProfileConfiguration{
-				SecurityTypes: cm.VirtualMachineSecurityTypesTrustedLaunch,
-			},
-		},
-		Tags: map[string]*string{
-			"arkloud-created-by": to.Ptr("cloudy-azure: vdo orchestrator - CreateSessionHost"),
-		},
-		UserID: "system",
-	}
-
-	// TODO: do we need a separate CreateSessionHostVirtualMachine function? vs CreateUserVirtualMachine?
-
-	// Create the session host VM
-	sessionHostVM, err := vdo.vmManager.CreateVirtualMachine(ctx, sessionHostVM)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create new session host: %w", err)
-	}
-
-	// retrieve host pool token
-	hostPoolToken, err := vdo.avdManager.RetrieveRegistrationToken(ctx, hostPoolName)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get host pool token: %w", err)
-	}
-
-	// build setup script
-	vdoConfig := vdo.config
-	vdoConfig.SaltMinionInstall = nil // disable salt minion install
-	script, err := vdo.buildVirtualMachineSetupScript(ctx, vdoConfig, hostPoolToken)
-	if err != nil {
-		return nil, logging.LogAndWrapErr(ctx, log, err, "Could not build powershell script (AVD enabled)")
-	}
-
-	// run the setup script on the session host VM
-	err = vdo.vmManager.ExecuteRemotePowershell(ctx, sessionHostVM.ID, script, 20*time.Minute, 15*time.Second)
-	if err != nil {
-		return nil, logging.LogAndWrapErr(ctx, log, err, "Could not run powershell (AVD enabled)")
-	}
-
-	// wait for the session host to be registered with the host pool and 'ready'
-	log.DebugContext(ctx, "Waiting for session host to be ready", "HostPoolName", hostPoolName, "VM", sessionHostVM.ID)
-	sessionHost, err := vdo.avdManager.WaitForSessionHost(ctx, hostPoolName, sessionHostVM.ID, 10*time.Minute)
-	if err != nil {
-		log.WarnContext(ctx, "Error waiting for session host", "Error", err)
-		return nil, logging.LogAndWrapErr(ctx, log, err, "Waiting for session host to be ready")
-	}
-
-	return sessionHost, nil
 }
